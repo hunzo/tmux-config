bf0cefb76c943b1d5b875de651d7c982949c5220
hunzo <takraw_s@hotmail.com>
2023-08-25 13:38:27 +0700

    update

--- install.sh
+++ install.sh
@@ -4,3 +4,8 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 cp tmux.conf ~/.tmux.conf
 
 tmux source ~/.tmux.conf
+
+sh -c "cat >> ~/.bashrc << EOF
+# add from script install tmux
+alias tmux='tmux -u'
+EOF"
