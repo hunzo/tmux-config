cbab28b9dcc64e4e9d4219bc436300a8a5c83571
surapong.n <surapong.n@nida.ac.th>
2024-04-10 15:19:07 +0700

    update

---
+++ install-linux.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+cp tmux.conf ~/.tmux.conf
+
+sudo cp ./tmux-mem-cpu-load /usr/local/bin
+
+tmux source ~/.tmux.conf
+
+sh -c "cat >> ~/.bashrc << EOF
+# add from script install tmux
+alias tmux='tmux -u'
+EOF"
