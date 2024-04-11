8a68805bf8a61158fcb5689087fc815d56c20d2f
surapong.n <surapong.n@nida.ac.th>
2024-04-11 13:56:24 +0700

    update

--- install-linux.sh
+++ install-linux.sh
@@ -6,11 +6,35 @@ cp tmux-linux.conf ~/.tmux.conf
 sudo cp ./tmux-mem-cpu-load /usr/local/bin
 
 tmux source ~/.tmux.conf
-
 sh -c 'cat >> ~/.bashrc << EOF
 # auto load tmux
-if command -v tmux &> /dev/null && [ -z "\$TMUX" ]; then
-    tmux -u attach -t default || tmux -u new -s default
-fi
+alias tmux="tmux -u"
 #-----------------------------
 EOF'
+
+
+# sh -c 'cat >> ~/.bashrc << EOF
+# # auto load tmux
+# if command -v tmux &> /dev/null && [ -z "\$TMUX" ]; then
+#     tmux -u attach -t default || tmux -u new -s default
+# fi
+# #-----------------------------
+# EOF'
+
+# sh -c 'cat >> ~/.bashrc << EOF
+# #--------------------------------------------------------
+# SESSION_NAME=sesh
+#
+# tmux has-session -t=\$SESSION_NAME 2> /dev/null
+#
+# if [[ \$? -ne 0 ]]; then
+#     TMUX="" tmux -u new-session -d -s "\$SESSION_NAME"
+# fi
+#
+# if [[ -z "\$TMUX" ]]; then
+#     tmux -u attach -t "\$SESSION_NAME"
+# else
+#     tmux switch-client -t "\$SESSION_NAME"
+# fi
+# #--------------------------------------------------------
+# EOF'
