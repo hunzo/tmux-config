3720c1408c7ec736059fd98fb655a049c108d5b9
surapong.n <surapong.n@nida.ac.th>
2024-04-11 13:18:20 +0700

    change config tmux

--- install-linux.sh
+++ install-linux.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 cp tmux-linux.conf ~/.tmux.conf
@@ -7,12 +7,10 @@ sudo cp ./tmux-mem-cpu-load /usr/local/bin
 
 tmux source ~/.tmux.conf
 
-sh -c "cat >> ~/.bashrc << EOF
-# add from script install tmux
-#-----------------------------
-alias tmux='tmux -u'
-if command -v tmux &> /dev/null && [ -z "$TMUX" ]; then
-    tmux attach -t default || tmux new -s default
+sh -c 'cat >> ~/.bashrc << EOF
+# auto load tmux
+if command -v tmux &> /dev/null && [ -z "\$TMUX" ]; then
+    tmux -u attach -t default || tmux -u new -s default
 fi
 #-----------------------------
-EOF"
+EOF'
