4383cacb8092ee82f2a90d7b0bb424aec528bc34
surapong.n <surapong.n@nida.ac.th>
2024-06-10 09:12:54 +0700

    update tmux

--- install-linux.sh
+++ install-linux.sh
@@ -3,7 +3,7 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 cp tmux-linux.conf ~/.tmux.conf
 
-sudo cp ./tmux-mem-cpu-load /usr/local/bin
+# sudo cp ./tmux-mem-cpu-load /usr/local/bin
 
 tmux source ~/.tmux.conf
 sh -c 'cat >> ~/.bashrc << EOF
